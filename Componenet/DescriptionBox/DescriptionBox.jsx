9db84dc2bfcbabbace6fe3aa241ff983525d7586
shiwani07 <158804155+shiwanithakur002@users.noreply.github.com>
2024-10-24 10:43:58 +0530

    Add files via upload

---
+++ Componenet/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+
+            <div className="descriptionbox-description">
+                <p>E-commerce is the process of buying and selling goods and services online. It has become increasingly popular in recent years, as it offers a convenient and efficient way to shop. There are many advantages to using e commerce, including the ability to compare prices, access a wider range of products, and enjoy greater convenience. </p>
+                <p> E-commerce is the buying and selling of goods or services over the internet. It has become an increasingly popular way to shop, as it is convenient and often cheaper than traditional brick-and-mortar stores. </p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default DescriptionBox
