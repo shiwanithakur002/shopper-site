ace492e51448fce003fadc4518801c646e15f3de
shiwani07 <158804155+shiwanithakur002@users.noreply.github.com>
2024-11-15 13:15:44 +0530

    Add files via upload

---
+++ Componenet/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const[menu,setmenu]=useState("shop")
+  return (
+    <div className='navbar'>
+
+      <div className="nav-logo">
+        <img src={logo} alt=" "/>
+        <p>WINGS</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li onClick={()=>{setmenu("shop")}}><Link style={{textDecoration:"none"}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>} </li>
+        <li onClick={()=>{setmenu("men")}}><Link style={{textDecoration:"none"}} to='/men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("women")}}><Link style={{textDecoration:"none"}} to='/women'>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("kids")}}><Link style={{textDecoration:"none"}} to='/kid'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt=''/></Link>
+        <div className="nav-cart-count"></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Pages/Shop.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../Componenet/Hero/Hero'
+import Popular from '../Componenet/Popular/Popular'
+import Offers from '../Componenet/Offers/Offers'
+import NewCollections from '../Componenet/NewCollections/NewCollections'
+import NewsLetter from '../Componenet/NewsLetter/NewsLetter'
+
+
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+    </div>
+  )
+}
+
+export default Shop
